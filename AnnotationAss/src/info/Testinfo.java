1791d481b68af7ddbd59e602c4f2f6f664cb3078
Rupasri-Namburi <105048426+Rupasri-Namburi@users.noreply.github.com>
2022-06-06 10:33:10 +0530

    Add files via upload

---
+++ AnnotationAss/src/info/Testinfo.java
@@ -0,0 +1,17 @@
+package info;
+
+public @interface Testinfo {
+public String developer() default"sheerin";
+
+public String Developer();
+
+public String DeveloperID();
+
+public String Supervisior();
+
+public String Date();
+
+public String Time();
+
+public String Description();
+}
