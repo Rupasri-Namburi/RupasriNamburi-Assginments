f588b62dfea3245ab801f74cf37586474ea11278
Rupasri-Namburi <105048426+Rupasri-Namburi@users.noreply.github.com>
2022-06-29 13:29:05 +0530

    Delete Spring Core directory

--- Spring Core/Q4/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.capgemini.springAssignment.Q4;
-
-public interface BankAccountRepository {
-    public double getBalance(long accountId);
-    public double updateBalance(long accountId, double newBalance);
-}
